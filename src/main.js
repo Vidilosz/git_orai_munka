7311154b77f7a5b47f62f0bced4693b63d73c29a
Petrik Diak <diak@petrik.hu>
2026-01-14 11:32:05 +0100

    javascript és néhány zöldesítés

--- src/main.js
+++ src/main.js
@@ -1,1 +1,8 @@
+const form = document.getElementById('registerForm')
+const successMessage = document.getElementById('successMessage')
 
+form.addEventListener('submit', (event) => {
+  event.preventDefault()       
+  form.reset()                  
+  successMessage.classList.remove('d-none') 
+})
